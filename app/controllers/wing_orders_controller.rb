c2d9ae2891bf81ae2816798d577cb35f12fabfa8
Evan Watterud <cod.videos@hotmail.com>
2016-04-29 23:27:45 -0400

    initial commit

---
+++ app/controllers/wing_orders_controller.rb
@@ -0,0 +1,58 @@
+class WingOrdersController < ApplicationController
+  def index
+    @wing_orders = WingOrder.all
+  end
+
+  def new
+    @wing_order = WingOrder.new
+    @state_collection = WingOrder::STATES
+    @quantity_collection = WingOrder::QUANTITIES
+    @flavor_collection = Flavor.all
+  end
+
+  def edit
+    @wing_order = WingOrder.find(params[:id])
+    @state_collection = WingOrder::STATES
+    @quantity_collection = WingOrder::QUANTITIES
+    @flavor_collection = Flavor.all
+  end
+
+  def update
+    @wing_order = WingOrder.find(params[:id])
+
+    if @wing_order.update_attributes(wing_order_params)
+      flash[:notice] = "Edit successful"
+      redirect_to wing_orders_path
+    else
+      flash[:alert] = @wing_order.errors.full_messages.first
+      render :edit
+    end
+  end
+
+  def create
+    @wing_order = WingOrder.new(wing_order_params)
+    @wing_order.flavors = Flavor.where(id: params[:wing_order][:flavor_ids])
+    if @wing_order.save
+      flash[:notice] = "Wing order created!"
+      redirect_to wing_orders_path
+    else
+      flash[:alert] = "Wing order not created"
+      @state_collection = WingOrder::STATES
+      @quantity_collection = WingOrder::QUANTITIES
+      @flavor_collection = Flavor.all
+      render :new
+    end
+  end
+
+  private
+
+  def wing_order_params
+    params.require(:wing_order).permit(
+      :customer_name,
+      :city,
+      :state,
+      :quantity,
+      :ranch_dressing
+    )
+  end
+end
